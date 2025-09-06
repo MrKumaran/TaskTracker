7cc4f880dd1def58d12887663b1cf080c2082f57
Kumaran S <mrkumarancse@gmail.com>
2025-09-07 00:12:48 +0530

    home controller

---
+++ src/main/java/in/project/tasktracker/Controller/Home.java
@@ -0,0 +1,60 @@
+package in.project.tasktracker.Controller;
+
+import in.project.tasktracker.Core.DBManager;
+import in.project.tasktracker.Core.ObjectBuilder;
+import in.project.tasktracker.Model.Task;
+import in.project.tasktracker.Model.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet(name = "Home", value = {"/", "/profile"})
+public class Home extends HttpServlet {
+    DBManager dbManager;
+
+    public void init() {
+        this.dbManager = DBManager.getInstance();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+//        System.out.println(session);
+        if (session == null) {
+            response.sendRedirect("/landing");
+        } else {
+            String path = request.getServletPath();
+            User user = dbManager.retrieveUser((String) session.getAttribute("user"));
+            request.setAttribute("user", user);
+            if (path.equals("/")) {
+                request.setAttribute("tasks", dbManager.retrieveUsersTasks(user.getUser_id()));
+                request.getRequestDispatcher("View/taskPage.jsp").forward(request, response);
+            } else if (path.equals("/profile")) {
+                request.getRequestDispatcher("View/index.jsp").forward(request, response);
+            }
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession(false);
+        if (session == null){
+            response.sendRedirect("/landing");
+        } else {
+            String path = request.getServletPath();
+            User user = dbManager.retrieveUser((String) session.getAttribute("user"));
+            if (path.equals("/newTask")) {
+                boolean isInserted;
+                Task task = ObjectBuilder.taskObjectBuilder(request, user.getUser_id());
+                if (task == null) isInserted = false;
+                else isInserted = dbManager.addTask(task);
+                response.setContentType("application/json");
+                response.getWriter().write("{\"status\": " + isInserted + "}");
+            }
+        }
+    }
+
+}
