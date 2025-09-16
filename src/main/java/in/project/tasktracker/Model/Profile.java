2ee67212811835d3fa52712284ae1d87381aaad4
Kumaran S <mrkumarancse@gmail.com>
2025-09-16 13:15:55 +0530

    New profile object to show details of user

---
+++ src/main/java/in/project/tasktracker/Model/Profile.java
@@ -0,0 +1,40 @@
+package in.project.tasktracker.Model;
+
+public class Profile {
+    private String userId;
+    private String userName;
+    private String mailId;
+    private String avatarURL;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAvatarURL() {
+        return avatarURL;
+    }
+
+    public void setAvatarURL(String avatarURL) {
+        this.avatarURL = avatarURL;
+    }
+
+    public String getMailId() {
+        return mailId;
+    }
+
+    public void setMailId(String mailId) {
+        this.mailId = mailId;
+    }
+}
