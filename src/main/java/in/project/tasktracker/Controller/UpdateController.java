46b53669bb1d12b3d4369a8e3b127a8c8ee191b8
Kumaran S <mrkumarancse@gmail.com>
2025-11-06 00:21:43 +0530

    New endpoint, enum used for operation, switch used instead of if-else

--- src/main/java/in/project/tasktracker/Controller/UpdateController.java
+++ src/main/java/in/project/tasktracker/Controller/UpdateController.java
@@ -3,7 +3,9 @@ package in.project.tasktracker.Controller;
 import in.project.tasktracker.Core.DBManager;
 import in.project.tasktracker.Core.EntityBuilder;
 import in.project.tasktracker.Enums.Auth.AuthEnum;
+import in.project.tasktracker.Enums.Error.ErrorEnum;
 import in.project.tasktracker.Enums.Profile.ProfileEnum;
+import in.project.tasktracker.Enums.Task.TaskEnum;
 import in.project.tasktracker.Model.Profile.ProfileUsernameUpdateDto;
 import in.project.tasktracker.Model.User.PasswordUpdateDto;
 import jakarta.servlet.ServletException;
@@ -36,27 +38,46 @@ public class UpdateController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String endpoint = request.getPathInfo();
-        String operation;
-        Boolean isOperationSuccess = false;
+        Enum operation;
+        boolean isOperationSuccess = false;
 
-        HttpSession session = request.getSession();
-        String userId = session.getAttribute("userId").toString();
+        HttpSession session = request.getSession(false);
+        String userId = session.getAttribute("user").toString();
 
-        if (endpoint.equals("/username")) {
-            ProfileUsernameUpdateDto profileUsernameUpdateDto = EntityBuilder.profileUsernameUpdateDtoBuilder(request, userId);
-            ProfileEnum profileEnum = dbManager.updateUserName(profileUsernameUpdateDto);
-            operation = profileEnum.toString();
-        } else if (endpoint.equals("/password")) {
-            PasswordUpdateDto passwordUpdateDto = EntityBuilder.passwordUpdateDtoBuilder(request, userId);
-            if (passwordUpdateDto != null) {
-                AuthEnum authEnum = dbManager.changePassword(passwordUpdateDto);
-                operation = authEnum.toString();
+        switch(endpoint){
+            case "/username" -> {
+                ProfileUsernameUpdateDto profileUsernameUpdateDto = EntityBuilder.profileUsernameUpdateDtoBuilder(request, userId);
+                operation = dbManager.updateUserName(profileUsernameUpdateDto);
+                isOperationSuccess = operation.equals(ProfileEnum.USERNAME_UPDATED);
             }
-            else operation = "NOT_VALID_OPERATION";
 
-        } else return;
+            case "/password" -> {
+                if (!request.getParameter("confirm-password").equals(request.getParameter("password"))){
+                    operation = AuthEnum.PASSWORD_NOT_MATCH;
+                } else {
+                    PasswordUpdateDto passwordUpdateDto = EntityBuilder.passwordUpdateDtoBuilder(request, userId);
+                    if (passwordUpdateDto != null) {
+                        operation = dbManager.changePassword(passwordUpdateDto);
+                        if (operation.equals(AuthEnum.CREDENTIALS_NOT_MATCH) || operation.equals(AuthEnum.USER_NOT_FOUND)) {
+                            response.sendRedirect("/logout", true);
+                            return;
+                        }
+                        isOperationSuccess = operation.equals(AuthEnum.PASSWORD_UPDATED);
+                    }
+                    else operation = ErrorEnum.PASSWORD_NOT_COMPLEX;
+                }
+            }
 
-        session.setAttribute("operation", operation);
+            case "/taskStatus" -> {
+                String taskId = request.getParameter("taskId");
+                operation = dbManager.updateTaskStatus(userId, taskId);
+                isOperationSuccess = operation.equals(TaskEnum.TASK_STATUS_UPDATED);
+            }
+
+            default -> operation = ErrorEnum.ILLEGAL_OPERATION_REQUEST;
+        }
+
+        session.setAttribute("operation", operation.toString());
         session.setAttribute("isOperationSuccess", isOperationSuccess);
         response.sendRedirect("/");
     }
