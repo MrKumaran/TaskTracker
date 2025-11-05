1fdb46dd863917e718495008e49cae82124f7291
Kumaran S <mrkumarancse@gmail.com>
2025-11-06 00:18:14 +0530

    New enum value

--- src/main/java/in/project/tasktracker/Enums/Auth/AuthEnum.java
+++ src/main/java/in/project/tasktracker/Enums/Auth/AuthEnum.java
@@ -3,6 +3,7 @@ package in.project.tasktracker.Enums.Auth;
 public enum AuthEnum {
     PASSWORD_UPDATED,
     PASSWORD_MATCH,
+    CREDENTIALS_NOT_MATCH,
     USER_NOT_FOUND,
     PASSWORD_NOT_MATCH,
     ERROR_OCCURRED
